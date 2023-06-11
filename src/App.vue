29304c0aebf6fec7818357bbef8250d1d309a56e
Adrian Wika <awika157@gmail.com>
2023-06-11 22:32:10 +0200

    Push navbar & home page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,109 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="context">
+    <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #031633">
+      <div class="container-fluid" id="spacer">
+        <a class="navbar-brand" href="#">
+          <img
+            src="/favicon.ico"
+            alt="Logo"
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+          />
+          Group Shopping App
+        </a>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav" id="navbarItems">
+            <li class="nav-item">
+              <RouterLink class="nav-link active" to="/">Strona główna</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Grupy zakupowe</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/">Moja strefa</RouterLink>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://fastly.picsum.photos/id/279/1920/1080.jpg?hmac=C8zPgdG1vyFSEXKiwZls3F3j_-OKLvNa0QUQ8UZ-8NU"
+                  alt="Logo"
+                  width="35"
+                  height="30"
+                  class="d-inline-block align-text-top"
+                  id="profilePic"
+                />
+              </a>
+              <ul class="dropdown-menu">
+                <li><div class="dropdown-item">Adrian Wika</div></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><RouterLink class="dropdown-item" to="/about">Ustawienia</RouterLink></li>
+                <li><RouterLink class="dropdown-item" to="/">Wyloguj</RouterLink></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="area">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
-  </header>
-
-  <RouterView />
+    <div id="spacer" class="vertcalSpacer"><RouterView /></div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+#spacer {
+  max-width: 1280px;
+  margin: 0 auto;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.vertcalSpacer {
+  padding-top: 5%;
+  padding-bottom: 5%;
+  min-height: 100vh;
 }
 
-nav {
+#navbarItems {
+  display: flex;
+  justify-content: end;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  font-size: large;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#profilePic {
+  border-radius: 10%;
 }
 </style>
