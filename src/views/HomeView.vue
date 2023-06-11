a8941d722edde6f5e6d10f951b739abe93aa43fe
Adrian Wika <awika157@gmail.com>
2023-06-11 18:51:03 +0200

    Push bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,6 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <!-- <button type="button" class="btn btn-primary">Primary</button> -->
   </main>
 </template>
