a82f0a1eb466ad8ab21bad6b9223808c917b2664
Adrian Wika <awika157@gmail.com>
2023-06-18 17:44:27 +0200

    push config

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import Vue from "vue";
+import Vue from "vue"
+import { BootstrapVue, } from 'bootstrap-vue'
+import App from "./App.vue"
+import router from "./router"
 
-import App from "./App.vue";
-import router from "./router";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "./assets/main.css"
 
-import "./assets/main.css";
+Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)  // import IconsPlugin
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount("#app")
